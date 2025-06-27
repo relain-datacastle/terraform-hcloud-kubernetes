3622331bf54a0714ef4960996381ce72c3409727
Philipp Müller <philipp@Mac.lan>
2025-06-27 16:16:51 +0200

    always generate kubeconfig

--- client.tf
+++ client.tf
@@ -40,10 +40,10 @@ resource "talos_cluster_kubeconfig" "this" {
 resource "terraform_data" "create_talosconfig" {
   count = var.cluster_talosconfig_path != null ? 1 : 0
 
-  triggers_replace = [
-    sha1(local.talosconfig),
-    var.cluster_talosconfig_path
-  ]
+  # triggers_replace = [
+  #   sha1(local.talosconfig),
+  #   var.cluster_talosconfig_path
+  # ]
 
   input = {
     cluster_talosconfig_path = var.cluster_talosconfig_path
@@ -85,10 +85,10 @@ resource "terraform_data" "create_talosconfig" {
 resource "terraform_data" "create_kubeconfig" {
   count = var.cluster_kubeconfig_path != null ? 1 : 0
 
-  triggers_replace = [
-    sha1(local.kubeconfig),
-    var.cluster_kubeconfig_path
-  ]
+  # triggers_replace = [
+  #   sha1(local.kubeconfig),
+  #   var.cluster_kubeconfig_path
+  # ]
 
   input = {
     cluster_kubeconfig_path = var.cluster_kubeconfig_path
