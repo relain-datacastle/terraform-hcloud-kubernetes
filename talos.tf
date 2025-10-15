a23ec0f50a194b1569390d883fe40afd4e68cd75
Mateusz Paluszkiewicz <theaifam5@gmail.com>
2025-10-15 23:20:29 +0200

    Remove unnecessary healthcheck invocation outside the loop
    Signed-off-by: Mateusz Paluszkiewicz <theaifam5@gmail.com>

--- talos.tf
+++ talos.tf
@@ -86,7 +86,6 @@ resource "terraform_data" "upgrade_control_plane" {
 
       if ${local.cluster_initialized}; then
         echo "Start upgrading Control Plane Nodes"
-        ${local.talos_healthcheck_enabled} && talosctl --talosconfig "$talosconfig" health --server -n "$host"
         set -- ${join(" ", local.control_plane_private_ipv4_list)}
         for host in "$@"; do
           talosctl --talosconfig "$talosconfig" upgrade -n "$host" --preserve --image '${local.talos_installer_image_url}'
