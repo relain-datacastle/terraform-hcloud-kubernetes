7d9172a8bcdb413c8615d0032ecc5874f24999f4
M4t7e <7535523+M4t7e@users.noreply.github.com>
2025-10-26 13:01:40 +0100

    Update Talos version to v1.11.1

--- variables.tf
+++ variables.tf
@@ -558,7 +558,7 @@ variable "packer_arm64_builder" {
 # Talos
 variable "talos_version" {
   type        = string
-  default     = "v1.11.3" # https://github.com/siderolabs/talos
+  default     = "v1.11.1" # https://github.com/siderolabs/talos
   description = "Specifies the version of Talos to be used in generated machine configurations."
 }
 
